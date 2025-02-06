44786f3ed08fc6c1d4e66723989f66aef43781d2
unknown <abdallaaljbour03@gmail.com>
2025-02-06 17:06:47 +0300

    create main page with the sidebar component with it styles the sizes is match the figme

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/sidebar.scss";
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="sidebar">
+      {/* Sidebar container, no items or submenus for now */}
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import "../styles/main.scss";
+import Sidebar from "../components/Sidebar";
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div className="main-content">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
