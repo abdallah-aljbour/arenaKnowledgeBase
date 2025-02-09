5f15320606bf2bb759d4d9343838c534295b29a3
unknown <abdallaaljbour03@gmail.com>
2025-02-09 21:05:34 +0300

    a

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,6 +4,8 @@ import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import EmptyComponent from "../components/empty";
 import Toolbar from "../components/toolBar";
+import CardGrid from "../components/CardGrid/CardGrid";
+import { CardData } from "../components/Card/types";
 
 const Main = () => {
   const [sidebarState, setSidebarState] = useState({
@@ -21,6 +23,16 @@ const Main = () => {
       hasSubmenuOpen: submenuOpen,
     });
   };
+  const cardsData: CardData[] = new Array(8).fill({
+    image: "/src/assets/Knowledge Base Thumbnil.svg",
+    title: "Collection Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    icons: [
+      { image: "/src/assets/folder-2.svg", text: "12 Section" },
+      { image: "/src/assets/note-text.svg", text: "50 Article" },
+    ],
+  });
 
   return (
     <div className="main-container">
@@ -30,6 +42,7 @@ const Main = () => {
           <Navbar />
           <EmptyComponent />
           <Toolbar />
+          <CardGrid cards={cardsData} />
         </div>
       </div>
     </div>
